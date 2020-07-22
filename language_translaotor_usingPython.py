d8bc76e7d1cf9de632ab843a8df8ad773c421119
AKHILDAS KS <akhildasks1999@gmail.com>
2020-07-22 19:39:02 +0530

    Update language_translaotor_usingPython.py

--- language_translaotor_usingPython.py
+++ language_translaotor_usingPython.py
@@ -2,6 +2,7 @@
 from googletrans import Translator
 #giving the input sentence which need to be translated.
 sentence = str(input("Sentence:"));
+#translator
 translator = Translator();
 translated_sentence = translator.translate(sentence,src='en',dest='language to be which the sentence should be translated');
 #to print the translated sentence.
